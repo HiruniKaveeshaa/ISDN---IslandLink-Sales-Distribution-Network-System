7229eac3c12f561083b7c14c711b21e0b7555d5b
Hiruni Kaveesha <hiruniiikaveesha@gmail.com>
2026-09-12 16:31:18 +0530

    Create InventoryRepository.java

---
+++ src/main/java/lk/ase/hiruni/islandlink/repository/InventoryRepository.java
@@ -0,0 +1,23 @@
+package lk.ase.kavinda.islandlink.repository;
+
+import lk.ase.kavinda.islandlink.entity.Inventory;
+import lk.ase.kavinda.islandlink.entity.Product;
+import lk.ase.kavinda.islandlink.entity.RDC;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProductAndRdc(Product product, RDC rdc);
+    
+    @Query("SELECT i FROM Inventory i WHERE i.rdc = :rdc")
+    List<Inventory> findByRdc(@Param("rdc") RDC rdc);
+    
+    @Query("SELECT i FROM Inventory i WHERE i.availableStock > 0")
+    List<Inventory> findAvailableStock();
+    
+    @Query("SELECT i FROM Inventory i WHERE i.product.id = :productId AND i.rdc.location = :rdcLocation")
+    Optional<Inventory> findByProductIdAndRdcLocation(@Param("productId") Long productId, @Param("rdcLocation") String rdcLocation);
+}
